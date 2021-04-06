aca37efd0435ca7915765a0616e055bfa01ed0a7
IharRubanik <74908305+IharRubanik@users.noreply.github.com>
2021-04-06 18:24:46 +0300

    fix: color, size, clean

--- script.js
+++ script.js
@@ -1,50 +1,57 @@
 let
-    canv = document.getElementById('canvas'),
-    ctx = canv.getContext('2d'),
-    isMouseDown = false,
-    input = document.getElementById('input'),
-    dot = document.getElementById('dot'),
-    v;
-    
-  
-    
-    
+  canv = document.getElementById('canvas'),
+  ctx = canv.getContext('2d'),
+  isMouseDown = false,
+  input = document.getElementById('input'),
+  dot = document.getElementById('dot'),
+  sizeValue,
+  color = document.getElementById('color'),
+  colorValue,
+  clear = document.getElementById('clear');
 
-    canv.width = window.innerWidth;
-    canv.height = window.innerHeight - 50;
-   
+canv.width = window.innerWidth;
+canv.height = window.innerHeight - 50;
 
+//code
+clear.addEventListener('click', function() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
 
-  //code
-  input.addEventListener('input', function() {
-    v = this.value;
- 
-  });
+color.addEventListener('input', function () {
+  colorValue = this.value;
+  dot.style.background = colorValue;
+});
 
+input.addEventListener('input', function () {
+  sizeValue = this.value;
+  dot.style.width = sizeValue * 2 + 'px';
+  dot.style.height = sizeValue * 2 + 'px';
 
+});
 
+canv.addEventListener('mousedown', function () {
+  isMouseDown = true;
+});
+canv.addEventListener('mouseup', function () {
+  isMouseDown = false;
+  ctx.beginPath();
+});
 
+canv.addEventListener('mousemove', function (e) {
+  if (isMouseDown) {
 
-  canv.addEventListener('mousedown', function() {
-    isMouseDown = true;
-  });
-  canv.addEventListener('mouseup', function(){
-    isMouseDown = false;
-    ctx.beginPath();
-  });
-  ctx.lineWidth = 10 * 2; 
-  canv.addEventListener ('mousemove', function(e) {
-      if ( isMouseDown ) 
-      {  
     ctx.lineTo(e.clientX, e.clientY);
     ctx.stroke();
+    ctx.lineWidth = sizeValue * 2;
+    ctx.fillStyle = colorValue;
+    ctx.strokeStyle = colorValue;
 
     ctx.beginPath();
-    ctx.arc(e.clientX, e.clientY, 10, 0, Math.PI * 2);
+    ctx.arc(e.clientX, e.clientY, sizeValue, 0, Math.PI * 2);
     ctx.fill();
 
     ctx.beginPath();
     ctx.moveTo(e.clientX, e.clientY);
-}
+  }
 
-  });
+});
